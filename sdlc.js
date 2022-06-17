58567797517582a7503feb1c75393aa7f757b7dc
Andrew Strong <andrewstrongdev@gmail.com>
2022-06-17 13:31:32 -0600

    sdlc commit

--- sdlc.js
+++ sdlc.js
@@ -11,6 +11,17 @@
 // Analysis of Requirements
 // Testing & Integration
 // Planning
+'--------------------------------------------------------------------------------------------------------------------------------------------------------'
+
+
+"Planning"
+"Analysis of Requirements"
+"Design"
+"Implementation"
+"Testing and Integration"
+"Maintain"
+
+
 
 
 //////////////////PROBLEM 2////////////////////
@@ -18,4 +29,12 @@
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
     The first one is done for you. 
 */
-const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added."
+const analysisOfReguirements = "This is the second step, in which the developers will figure out all necessary aspects of the software that they will develop."
+const design = "This is the third step, where a general design for the software is layed out."
+const implementation = "This is the fourth step, where software developers will actually code the software, taking into account everything predetermined in the first three phases of the SLDC"
+const testingAndIntegration = "This is the fifth step, where quality assurance will test for bugs and inconsistencies in the software. Developers will integrate the information they recieve from testing to imporove the product before it is initially released."
+const maintain = "This is the sixth step, where developers will fix bugs that they didnt catch, or release software upgrades after the software has already been released and used by the intended end user."
+
+
+
