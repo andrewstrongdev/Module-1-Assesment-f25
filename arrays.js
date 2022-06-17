c92fb037d2b5c94a39ef3778edfeeb1dd4150fb4
Andrew Strong <andrewstrongdev@gmail.com>
2022-06-17 13:48:00 -0600

    arrays commit

--- arrays.js
+++ arrays.js
@@ -4,11 +4,14 @@
 const faveColors = ['red', 'green', 'black']
 
 //CODE HERE
+const colorCopy = faveColors.slice(0,3)
 
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
 //CODE HERE
+colorCopy.push('yellow')
+//console.log(colorCopy)
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -16,9 +19,10 @@ const faveColors = ['red', 'green', 'black']
   numbers array that will capture only the middle numbers (2, 3, 4).
 */
 
-const numbers = [1, 2, 3, 4, 5]
-
 //CODE HERE
+const numbers = [1, 2, 3, 4, 5]
+const middleNums = numbers.slice(1,4)
+//console.log(middleNums)
 
 
 //////////////////PROBLEM 4////////////////////
@@ -34,3 +38,12 @@ const numbers = [1, 2, 3, 4, 5]
 const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
 
 // CODE HERE
+const answers = []
+for (let i = 0; i < bigOrSmallArray.length; i++) {
+  if (bigOrSmallArray[i] > 100){
+    answers.push('big')
+  } else{
+    answers.push('small')
+  }
+}
+//console.log(answers)
